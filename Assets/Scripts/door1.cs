c4eaa8b138359d366d4e268de2775fd1eee2ebd8
Marvin <marvin.k.scharding@gmail.com>
2020-02-10 14:26:54 +0100

    proto level 3 working

--- Assets/Scripts/door1.cs
+++ Assets/Scripts/door1.cs
@@ -5,30 +5,58 @@ using UnityEngine;
 public class door1 : MonoBehaviour
 {
     [SerializeField]GameObject upPosition;
+    Vector3 initialPosition;
     bool activated = false;
+    bool closed = false;
     // Start is called before the first frame update
+    
     void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
+    enum State
+    {
+        IDLE,
+        OPENING,
+        CLOSING
+    }
+    State state = State.IDLE;
     // Update is called once per frame
     void Update()
     {
-        if (activated)
+
+        switch(state)
         {
-            Open();
+            case State.IDLE:
+
+                break;
+            case State.OPENING:
+                Open();
+                break;
+            case State.CLOSING:
+                close();
+                break;
         }
     }
      void Open()
     {
-        if(transform.position.y < upPosition.transform.position.y)
-        {
-            transform.position = Vector3.Lerp(transform.position, upPosition.transform.position, Time.deltaTime);
-        }
+        transform.position = Vector3.Lerp(transform.position, upPosition.transform.position, Time.deltaTime);  
     }
     public void Activate()
     {
-        activated = true;
+        state = State.OPENING;
+    }
+    public void Closing()
+    {
+        state = State.CLOSING;
+    }
+    void close ()
+    {
+        transform.position = Vector3.Lerp(transform.position, initialPosition, Time.deltaTime);
+        if (transform.position.y - initialPosition.y < 0.1f)
+        {
+            state = State.IDLE;
+        }
     }
 }
