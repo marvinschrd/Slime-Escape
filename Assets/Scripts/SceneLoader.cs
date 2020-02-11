ed015c1883a5ac1bb813723529247f9cd6cdb280
Kevincouper <fawderfox@gmail.com>
2020-02-11 15:40:49 +0100

    Created end for level 1, 2, 3

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    [SerializeField] String nextLevel;
+    [SerializeField] String currentLevel;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(nextLevel);
+        SceneManager.UnloadSceneAsync(currentLevel);
+        _spriteRenderer.color=Color.red;
+        
+    }
+    
+    
+    
+    
+}
