e94e3f2fdfc8b734b16749471c6fc145b477c240
Kevincouper <fawderfox@gmail.com>
2020-02-12 15:17:47 +0100

    All menus added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject panelMenuPause;
+    [SerializeField] GameObject panelMenuWin;
+    [SerializeField] GameObject panelMainMenu;
+    [SerializeField] String reloadScene;
+    [SerializeField] String mainMenuScene;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (panelMenuPause.activeSelf)
+            {
+                UnloadMenuPause();
+            }
+            else
+            {
+                LoadMenuPause();
+            }
+        }
+        
+        ////////////
+        /// Make win condition
+        ////////////
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
+        Time.timeScale = 0;
+    }
+    public void UnloadMainMenu()
+    {
+        panelMainMenu.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void LoadMenuPause()
+    {
+        panelMenuPause.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void UnloadMenuPause()
+    {
+        panelMenuPause.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void LoadMenuWin()
+    {
+        panelMenuWin.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("quit");
+    }
+    public void Retry()
+    {
+        SceneManager.LoadScene(reloadScene);
+    }
+}
